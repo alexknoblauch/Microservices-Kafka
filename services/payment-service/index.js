d01f8113c5685138384216388d5b19b9a3a774e2
Alex Knoblauch <your-email@example.com>
2026-01-05 14:16:23 -0600

    PaymentService try Catch Block, kafka withRetry, uuid für paymendId

--- services/payment-service/index.js
+++ services/payment-service/index.js
@@ -5,6 +5,10 @@ import express from 'express'
 import cors from 'cors'
 import { Kafka } from 'kafkajs'
 import { connectToKafka } from '../kafka/helpers/connectToKafka'
+import { v4 as uuidv4 } from 'uuid';
+import { withRetry } from '../../helpers/withRetry';
+import logger from '../../lib/winston';
+
 
 //
 //KAFKA
@@ -30,18 +34,38 @@ app.post('/payment-service', async (req, res) => {
     // ASSUME THAT WE GET THE COOKIE AND BCRYPTED USERID
 
     const userId = '123'
+    const paymentId = uuidv4()
 
     //TODO: STRIPE
 
     // KAFKA
-    producer.send({
-        topic: 'payments',                       // sends message to paymnets Topic in Kafka
-        messages: [
-            {value: JSON.stringify({ cart, userId })}
-        ]
-    })
+    try{
+        await withRetry(async () => {
+           await producer.send({
+                topic: 'payments',                       // sends message to paymnets Topic in Kafka
+                messages: [
+                    {value: JSON.stringify({ cart, userId, paymentId })}
+                ]
+            })
+        }, 3)
+        return res.status(200).send('payment successfull')
 
-    return res.status(200).send('payment successfull')
+    } catch(err){
+        logger.error('Payment Producer failed', {
+            err,
+            cart, 
+            userId, 
+            paymentId
+        })
+
+        return res.status(500).json({
+            success: false,
+            error: 'PAYMENT_PROCESSING_FAILED',  
+            message: 'Payment could not be processed. Please try again.',
+            paymentId: paymentId,                 
+            timestamp: new Date().toISOString()
+        })
+    }
 })
 
 app.use((err, req, res, next) => {
